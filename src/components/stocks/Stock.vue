450d6ca67c5d50b10e94c6c194a72b5223e87ecd
Sharad Shinde <shindesharad71@gmail.com>
2017-08-01 22:27:22 +0530

    stocks component added

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>Stocks/Stock Component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">NAME</h3>
+                <small>(Price: Price)</small>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input type="number" class="form-control" placeholder="Quantity">
+                </div>
+                <div class="pull-right">
+                    <button class="btn btn-success">Buy</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>Stocks Component</h1>
+<div>
+    <app-stock v-for="stock in stocks"></app-stock>
+</div>
 </template>
+
+<script>
+import Stock from './Stock.vue';
+
+export default {
+  data () {
+      return {
+          stocks: [
+              {id:1, name: 'BMW', price: 100},
+              {id:2, name: 'ABC', price: 150},
+              {id:3, name: 'XYZ', price: 190}
+          ]
+      }
+    },
+    components: {
+        'app-stock': Stock
+    }
+}
+</script>
