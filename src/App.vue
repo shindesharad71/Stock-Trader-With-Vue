c49bc5b7c282c8c7c3f9e754535a4807ec272d3e
Sharad Shinde <shindesharad71@gmail.com>
2017-08-01 20:09:41 +0530

    nav added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
+<div>
+    <app-header></app-header>
     <div class="container">
-        <router-view></router-view>
+        <div class="row">
+            <router-view></router-view>
+        </div>
     </div>
+</div>
 </template>
 
 <script>
-
+    import Header from './components/Header.vue';
     export default {
-
+        components: {
+            'app-header': Header
+        }
     }
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,35 @@
 <template>
-    <h1>Header Component</h1>
+    <nav class="navbar navbar-light red">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <router-link to="/portfolio" active-class="active" tag="li"><a>Portfolio</a></router-link>
+                <router-link to="/stocks" active-class="active" tag="li"><a>Stocks</a></router-link>                
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">End Day</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save & Load <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Save Data</a></li>
+                  <li><a href="#">Load Data</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
 </template>
+
+<style scoped>
+.navbar a{
+    color: #fff;
+}
+
+.navbar a:hover{
+    color: #000;
+}
+</style>
