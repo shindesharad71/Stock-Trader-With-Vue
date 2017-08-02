53cc8d3e14c3554bbb5b7dfdc016f28ab48635ca
Sharad Shinde <shindesharad71@gmail.com>
2017-08-03 02:43:56 +0530

    vue resorce setup added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 import { routes } from './routes';
 import store from './store/store';
 
+Vue.use(VueResource);
 Vue.use(VueRouter);
+
+Vue.http.options.root = 'https://vue-http-1ea3a.firebaseio.com/'; // DB Address from Firebase
 
 Vue.filter('currency', (value) => {
     return '$' + value.toLocaleString();
