6fbfd44aeb8902b764120a4ea3a6c350eeafdcfd
Sharad Shinde <shindesharad71@gmail.com>
2017-08-02 23:47:47 +0530

    funds added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,6 +9,7 @@
                 <router-link to="/portfolio" active-class="active" tag="li"><a>Portfolio</a></router-link>
                 <router-link to="/stocks" active-class="active" tag="li"><a>Stocks</a></router-link>                
             </ul>
+            <strong class="navbar-text navbar-right">Funds: {{funds}}</strong>
             <ul class="nav navbar-nav navbar-right">
               <li><a href="#">End Day</a></li>
               <li class="dropdown">
@@ -23,6 +24,17 @@
         </div><!--/.container-fluid -->
       </nav>
 </template>
+
+<script>
+export default {
+	computed: {
+		funds() {
+			return this.$store.getters.funds;
+		}
+	}
+}
+</script>
+
 
 <style scoped>
 .navbar a{
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>Home Component</h1>
+<div>
+    <h1>Trade or View Portfolio</h1>
+    <h6>You May Save & Load Your Data</h6>
+    <h6>Click on 'End Day' To Begain New Day!</h6>
+    <hr>
+    <p>Your Funds: {{funds}}</p>
+</div>
 </template>
+
+<script>
+export default {
+    computed: {
+        funds() {
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
+
