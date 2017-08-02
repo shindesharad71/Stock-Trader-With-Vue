9ca322b36ad107862cb45d36b487c2335e93c0d3
Sharad Shinde <shindesharad71@gmail.com>
2017-08-03 02:32:32 +0530

    dropdown open close set by vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,9 +12,10 @@
             <strong id="funds" class="navbar-text navbar-right">Funds: {{funds | currency }}</strong>
             <ul class="nav navbar-nav navbar-right">
               <li><a href="#" @click="endDay">End Day</a></li>
-              <li class="dropdown">
+              <li class="dropdown" :class="{ open : isDropdownOpen }"
+			  @click="isDropdownOpen = !isDropdownOpen">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save & Load <span class="caret"></span></a>
-                <ul class="dropdown-menu">
+                <ul class="dropdown-menu" id="drop">
                   <li><a href="#">Save Data</a></li>
                   <li><a href="#">Load Data</a></li>
                 </ul>
@@ -29,6 +30,11 @@
 import { mapActions } from 'vuex';
 
 export default {
+	data () {
+		return {
+			isDropdownOpen: false
+		}
+	},
 	computed: {
 		funds() {
 			return this.$store.getters.funds;
@@ -49,6 +55,10 @@ export default {
 <style scoped>
 .navbar a{
     color: #fff;
+}
+
+#drop a{
+    color: #000;
 }
 
 .navbar a:hover, .navbar a:active{
