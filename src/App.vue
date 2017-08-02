4389fa98dca7808d5e678de5bcf3d2d1d3663db6
Sharad Shinde <shindesharad71@gmail.com>
2017-08-03 02:23:28 +0530

    routing animations added

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <app-header></app-header>
     <div class="container">
         <div class="row">
-            <router-view></router-view>
+            <transition name="slide" mode="out-in">
+                <router-view></router-view>
+            </transition>
         </div>
     </div>
 </div>
@@ -22,5 +24,33 @@
 </script>
 
 <style>
+.slide-enter-active {
+    animation: slide-in 200ms ease-out forwards;
+}
 
+.slide-leave-active {
+    animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+    from {
+        transform: translateY(-30px);
+        opacity: 0
+    }
+    to {
+        transform: translateY(0);
+        opacity: 1;
+    }
+}
+
+@keyframes slide-out {
+    from {
+        transform: translateY(0);
+        opacity: 1;
+    }
+    to {
+        transform: translateY(-30px);
+        opacity: 0
+    }
+}
 </style>
