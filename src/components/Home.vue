0556f3ed24369c5f99ed3b0f78c5abe6bbd07601
Sharad Shinde <shindesharad71@gmail.com>
2017-08-03 17:10:45 +0530

    icons added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,13 @@
 <template>
 <div>
-    <h1>Trade or View Portfolio</h1>
-    <h6>You May Save & Load Your Data</h6>
-    <h6>Click on 'End Day' To Begain New Day!</h6>
+    <h1><i class="fa fa-line-chart" aria-hidden="true"></i> Stock Trader Made With Vue JS 2<br>
+    <small><i class="fa fa-user" aria-hidden="true"></i> By Sharad Shinde</small></h1>
+    <br>
+    <h3>Features</h3>
+    <li>You May Save & Load Your Data</li>
+    <li>Click on 'End Day' To Begain New Day!</li>
     <hr>
+    <br>
     <p>Your Funds: {{ funds | currency }}</p>
 </div>
 </template>
